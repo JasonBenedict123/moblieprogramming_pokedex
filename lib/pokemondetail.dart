01601379ba2b95ea97ec5680d498b8009ee2d960
jason benedict <jasonbenedict2001@gmail.com>
2021-02-16 18:50:52 +0700

    Pokedex new

--- lib/pokemondetail.dart
+++ lib/pokemondetail.dart
@@ -47,7 +47,7 @@ class PokeDetail extends StatelessWidget {
                   style: TextStyle(fontWeight: FontWeight.bold)),
               Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: pokemon.nextEvolution == null? <Widget> [Text("This is the final form kali")]
+                  children: pokemon.nextEvolution == null? <Widget> [Text("This is the final form")]
                       : pokemon.nextEvolution
                       .map((n) => FilterChip(backgroundColor: Colors.green,
                       label: Text(n.name, style: TextStyle(color: Colors.white)), onSelected: (b){})).toList()
